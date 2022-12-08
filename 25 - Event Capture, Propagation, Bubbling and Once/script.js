94673b3bdc471c6dc5cf3814e27740fa97dc6196
Joel D'Autilio <joeldautilio@gmail.com>
2022-12-08 20:19:51 -0300

    Complete 25

---
+++ 25 - Event Capture, Propagation, Bubbling and Once/script.js
@@ -0,0 +1,16 @@
+const divs = document.querySelectorAll('div');
+divs.forEach(div => {
+  div.addEventListener(
+    'click', 
+    logText, 
+    {
+      capture: false,
+      once: true
+    }
+  );
+})
+
+function logText(e) {
+  console.log(this.classList.value);
+  // e.stopPropagation();
+}
